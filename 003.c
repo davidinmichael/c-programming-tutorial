08f7279dc1286ef4fc2b3f99258f3bdb12a9cdbf
mayriann <mayrianna4real@gmail.com>
2023-01-19 13:09:38 -0700

    Task 3

---
+++ 003.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+   int num;
+   int num1;
+   
+   printf ("Enter your number: ");
+   scanf("%c", &num);
+
+    if (num == num1 == 0)
+    { 
+        printf ("UserInput is even\n"); 
+    }
+    
+    if (num / num1 == 1 )
+    {
+    printf ("UserInput is odd");
+    }  
+    
+   if (num == 0)
+   {
+       printf ( "This is Zero");
+   }
+   return (0);
