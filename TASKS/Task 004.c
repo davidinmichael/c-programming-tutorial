ede716bce2f9c8bb590f461e1f0f06dd0d81b45c
seurcoding <mbiseg108@gmail.com>
2023-01-19 01:18:31 -0800

    task 004

--- TASKS/Task 004.c
+++ TASKS/Task 004.c
@@ -12,3 +12,25 @@ Output:
 Your name is David and you are 65 years old.
 
 */
+
+
+
+#include <stdio.h>
+
+int main()
+{
+
+ int age;
+ int name[6];
+
+printf("what is your name: ");
+scanf("%s", &name);
+ {
+printf("How old are you: ");
+scanf("%d", &age);
+}{
+printf("Your name is %s and your are %d old \n", name, age);
+}
+return 0;
+}
+
