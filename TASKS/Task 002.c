a5a9784547ee6e4259ce1bbdc788153ed432516b
seurcoding <mbiseg108@gmail.com>
2023-01-19 00:07:04 -0800

    Task 002

--- TASKS/Task 002.c
+++ TASKS/Task 002.c
@@ -4,3 +4,23 @@ If the score is less than 80, display to the user that they
 are not elgible to be enrolled.
 If the score is greater than or equal 80, they can be enrolled.
 */
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter your score: ");
+    scanf("%d",&num);
+
+    if(num>=80)
+        printf("You are eligbe to be enrolled");
+    else
+        {
+        printf("You are not eligbe to be enrolled");
+        }
+    return 0;
+}
+
