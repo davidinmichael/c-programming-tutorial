e653f1a019afd3790e1d39ed1308c40c7277176a
funxal <habibah049@gmail.com>
2023-01-19 16:56:18 +0000

    this task

--- TASKS/Task 003.c
+++ TASKS/Task 003.c
@@ -7,3 +7,21 @@ If the number is zero, display 'This is zero'
 NOTE: 'userInput' should be the number entered by the user.
 
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int userInput;
+
+	printf("Please enter your magic number ");
+	scanf("%d", &userInput);
+
+	if ((userInput % 2) == 0)
+		printf("Your magic number is even!\n");
+	else if ((userInput % 2) == 1)
+		printf("Your magic number is odd!\n");
+	else
+		printf("This is zero\n");
+	return (0);
+}
--- TASKS/Task 004.c
+++ TASKS/Task 004.c
@@ -1,5 +1,5 @@
 /* Write your program after the comment
-Write a program that asks and reads the following input from a use;
+Write a program that asks and reads the following input from a user;
 Your name
 Your age
 and then displays; Your name is 'name' and you are 'age' years old.
@@ -12,3 +12,18 @@ Output:
 Your name is David and you are 65 years old.
 
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char name[10];
+	int age, y;
+
+	printf("What is your name?\nHow old are you? \n");
+	scanf("%s %d", name,  &age);
+
+	printf("Your name is %s and you are %d years old.\n", name, age);
+	return (0);
+}
+
