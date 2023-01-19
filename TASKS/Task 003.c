1b8237e099a50f6fff1c8ed3b42994d607dfd8be
seurcoding <mbiseg108@gmail.com>
2023-01-19 00:35:47 -0800

    task003

--- TASKS/Task 003.c
+++ TASKS/Task 003.c
@@ -7,3 +7,25 @@ If the number is zero, display 'This is zero'
 NOTE: 'userInput' should be the number entered by the user.
 
 */
+
+#include <stdio.h>
+
+int main()
+    {
+    int Number;
+
+    printf("Enter your Number: ");
+    scanf("%d",&Number);
+
+    if(Number%2==0)
+        {
+            printf("Entered number is even\n");
+    }
+    else
+        {
+
+    printf("Entered number is odd\n");
+       }
+    return 0;
+    }
+
