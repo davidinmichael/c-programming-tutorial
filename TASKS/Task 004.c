a4bba003bdad0feca708a4518e558faa18f8519f
Sampul-CodeMine <studysampul@gmail.com>
2023-01-20 06:50:08 +0100

    Solution to task 4 - Dukeson

--- TASKS/Task 004.c
+++ TASKS/Task 004.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Write your program after the comment
-Write a program that asks and reads the following input from a use;
-Your name
-Your age
-and then displays; Your name is 'name' and you are 'age' years old.
-
-Example;
-what is your name: David
-How old are you?: 65
-
-Output:
-Your name is David and you are 65 years old.
-
-*/
+/**
+ * Write your program after the comment
+ * Write a program that asks and reads the following input from a use;
+ * Your name
+ * Your age
+ * and then displays; Your name is 'name' and you are 'age' years old.
+ *
+ * Example;
+ *
+ * what is your name: David
+ * How old are you?: 65
+ *
+ * Output:
+ * Your name is David and you are 65 years old.
+ */
 
 int main(void)
 {
