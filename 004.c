4f78a841c4042e90afac67c300759b767c800d48
mayriann <mayrianna4real@gmail.com>
2023-01-19 17:14:18 -0700

    Task 4

---
+++ 004.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+/* A program that asks, reads and displays the name, age of a user.
+ * Name is David
+ * Age is 65
+*/
+
+int main()
+{
+	int name = David;
+	int age = 65;
+
+	printf("What is your name: \n");
+	scanf("%d", name);
+
+	printf("What is your age: \n");
+	scanf("%d", age);
+
+	printf("Your name is %d and you are %d years old");
+
+	return 0;
+
+}
+
