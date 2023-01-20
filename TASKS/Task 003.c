8b1718b6bf14130ec913a23189ed211ec9a2b6c0
Sampul-CodeMine <studysampul@gmail.com>
2023-01-20 06:47:10 +0100

    Solution to task 3 - Dukeson

--- TASKS/Task 003.c
+++ TASKS/Task 003.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Write your program after the comment
-Write a program that reads input from user (Number)
-If the number is Odd, display 'userInput is Odd'
-If the number is even, display 'userInput is even'
-If the number is zero, display 'This is zero'
-
-NOTE: 'userInput' should be the number entered by the user.
-
-*/
+/**
+ * Write your program after the comment
+ * Write a program that reads input from user (Number)
+ * If the number is Odd, display 'userInput is Odd'
+ * If the number is even, display 'userInput is even'
+ * If the number is zero, display 'This is zero'
+ *
+ * NOTE: 'userInput' should be the number entered by the user.
+ */
 
 int main(void)
 {
-        int num = 0, result = 0;
+        int num = 0;
 
         printf("Enter Number:\t");
         scanf("%d", &num);
