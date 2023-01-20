5134bf2972dba8039ec8e0fb805b7ee19226b377
Sampul-CodeMine <studysampul@gmail.com>
2023-01-20 06:43:34 +0100

    Solution to task 2 - Dukeson

--- TASKS/Task 002.c
+++ TASKS/Task 002.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Write your program after the comment
-Write a program that asks and reads the score of a user
-If the score is less than 80, display to the user that they
-are not elgible to be enrolled.
-If the score is greater than or equal 80, they can be enrolled.
-*/
+/**
+ * Write your program after the comment
+ * Write a program that asks and reads the score of a user
+ * If the score is less than 80, display to the user that they
+ * are not elgible to be enrolled.
+ * If the score is greater than or equal 80, they can be enrolled.
+ */
 
 int main(void)
 {
@@ -18,6 +19,6 @@ int main(void)
         if (score < 80)
                 printf("You cannot be enrolled.");
         else if (score >= 80)
-                printf("You csn be enrolled.");
+                printf("You can be enrolled.");
         return (0);
 }
