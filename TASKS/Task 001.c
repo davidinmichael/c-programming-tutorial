df19d52b04ca829865c4e0b34f7770b18b73597b
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-01-20 04:28:43 +0100

    Update Task 001.c

--- TASKS/Task 001.c
+++ TASKS/Task 001.c
@@ -1,3 +1,8 @@
+ /* Write your program after the comment
+Write a program that prints the alphabet in lowercase, followed by a new line.
+Print all the letters except q and e
+use printf
+*/
 #include <stdio.h>
 
 int main() {
