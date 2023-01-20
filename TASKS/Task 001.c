e8fef3aee4a003e1f8919a7d2146721b42524718
Sampul-CodeMine <studysampul@gmail.com>
2023-01-20 05:59:09 +0100

    Update Task 001.c

--- TASKS/Task 001.c
+++ TASKS/Task 001.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /* Write your program after the comment
 Write a program that prints the alphabet in lowercase, followed by a new line.
 
@@ -5,3 +8,17 @@ Print all the letters except q and e
 
 use printf
 */
+
+int  main(void)
+{
+        char alpha;
+
+        for (alpha = 'a'; alpha <= 'z'; alpha++)
+                printf("%c", alpha);
+        printf("\n");
+}
+
+
+
+
+
