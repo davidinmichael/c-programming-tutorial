05f370d21a491118edc4b0c8daf357116e93b245
BEChukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-01-21 21:48:23 +0100

    Update Task 005.c

--- TASKS/Task 005.c
+++ TASKS/Task 005.c
@@ -2,3 +2,22 @@
  Write a program in C to find the square of any number
 using the function.
 */
+
+#include <stdio.h>
+
+int sqr(int num)
+{
+    return num * num;
+}
+int main()
+{
+    int num, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    result = sqr(num);
+    printf("Square of %d is %d", num, result);
+
+    return 0;
+}
