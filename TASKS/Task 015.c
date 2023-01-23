b542c7b3f5cb8103ac48f57f0c9edad33282361b
BEChukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-01-23 09:52:23 +0100

    Update Task 015.c

--- TASKS/Task 015.c
+++ TASKS/Task 015.c
@@ -14,11 +14,11 @@ void FizzBuzz()
     int n;
     for (n = 1; n <= 100; n++)
     {
-        if (n % 3 == 0)
+        if (n % 3 == 0 && n % 5 != 0)
         {
             printf("Fizz ");
         }
-        else if (n % 5 == 0)
+        else if (n % 5 == 0 && n % 3 !=0)
         {
             printf("Buzz ");
         }
