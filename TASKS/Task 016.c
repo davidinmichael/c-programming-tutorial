2c8e3e76546c8fcb27da39e17e9c7641fe70dfb5
seurcoding <mbiseg108@gmail.com>
2023-01-27 00:26:09 -0800

    recursive tsk

--- TASKS/Task 016.c
+++ TASKS/Task 016.c
@@ -9,3 +9,33 @@
     Output:
     720         // That is 1x2x3x4x5x6 = 720
 */
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int factor_Number(int n)
+{
+
+    if (n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*(factor_Number(n-1));
+    }
+}
+
+
+int main ()
+{
+    int n=6;
+
+
+        printf("%d ",factor_Number(n));
+
+    return 0;
+}
