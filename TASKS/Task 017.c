e95f2fe0d9bce4fb080ae35d2950f96a5720c748
Dukeson Ehigboria O <studysampul@gmail.com>
2023-01-27 09:09:33 +0100

    Update Task 017.c

--- TASKS/Task 017.c
+++ TASKS/Task 017.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /*
     Write a recursive function that takes an integer as
     an argument and returns the sum of all integers from
@@ -9,3 +11,27 @@
     Output:
     21      // That is; 1+2+3+4+5+6 = 21
 */
+
+int add(int);
+
+int main() {
+    int num;
+    
+    num = 6;
+    printf("%d\n", add(num));
+    return 0;
+}
+
+/**
+ * add - function to recursively add a number till it is 1
+ * @num - number to add till it is 1
+ * Return: the recursive sum
+ */
+int add(int num)
+{
+    if (num == 1)
+        return (num);
+    if (num < 1)
+        return (0);
+    return ( num + add(num -1));
+}
