6474701353c465f0704e3eb4b54533daafaa8b35
seurcoding <mbiseg108@gmail.com>
2023-01-27 00:47:17 -0800

    power number

--- TASKS/Task 018.c
+++ TASKS/Task 018.c
@@ -3,3 +3,26 @@
 
     functionName(int x, int y);
 */
+
+
+#include <stdio.h>
+
+int power(int base, int a)
+{
+    if (a != 0)
+        return (base * power(base, a - 1));
+    else
+        return 1;
+}
+
+int main ()
+{
+    int base, a, result;
+    printf("Enter base number: ");
+    scanf("%d", &base);
+    printf("Enter power number(positive integer): ");
+    scanf("%d", &a);
+    result = power(base, a);
+    printf("%d^%d = %d", base, a, result);
+    return 0;
+}
