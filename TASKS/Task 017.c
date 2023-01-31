8fe1f5a6833cdace7ecdad01dd35876792fed7e8
DevOse <abuluoisereme@gmail.com>
2023-01-31 11:34:18 -0800

    solved tasks 16 and 17

--- TASKS/Task 017.c
+++ TASKS/Task 017.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /*
     Write a recursive function that takes an integer as
     an argument and returns the sum of all integers from
@@ -9,3 +11,27 @@
     Output:
     21      // That is; 1+2+3+4+5+6 = 21
 */
+
+/**
+ * main - Check the function
+ * sum: sums all integers from 1 to n
+ * Return: 0
+ */
+
+int sum(int n)
+{
+	if (n < 1)
+		return (0); //base case
+	else
+	{
+		return (n + sum(n - 1));
+	}
+}
+
+int main()
+{
+	int x = sum(6);
+
+	printf("%d\n", x);
+	return (0);
+}
--- TASKS/Task 018.c
+++ TASKS/Task 018.c
@@ -1,5 +1,16 @@
+#include <stdio.h>
+
 /*
     Write a function that returns the value of x raised to the power of y.
 
     functionName(int x, int y);
 */
+
+/**
+ * main - Tests the function
+ * power: returns value of x raised to the power of y
+ * Return: 0
+ */
+
+int power(int x, int y)
+
