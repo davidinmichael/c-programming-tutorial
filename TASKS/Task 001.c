199f89d9f7df367c20728590141b287022e56d83
JCTNIG <108359199+JCTNIG@users.noreply.github.com>
2023-02-05 19:14:32 +0100

    Update Task 001.c

--- TASKS/Task 001.c
+++ TASKS/Task 001.c
@@ -5,3 +5,19 @@ Print all the letters except q and e
 
 use printf
 */
+
+#include <stdio.h>
+
+int main() 
+{
+    
+    
+    for ( char i = 'a'; i <= 'z' ; i++)
+    {
+        if (i != 'e' && i != 'q')
+        {
+            printf("%c\n", i);
+        }
+    }
+return (0);
+}
