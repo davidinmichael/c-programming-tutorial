f02d14688475350aca3aa537c91c5191e191228b
JCTNIG <108359199+JCTNIG@users.noreply.github.com>
2023-02-06 23:19:21 +0100

    Update Task 009.c

--- TASKS/Task 009.c
+++ TASKS/Task 009.c
@@ -3,3 +3,29 @@ Write a function that checks for lowercase character.
 That is, when passed in an arguments, it checks if the
 argument is lowercase or uppercase.
 */
+
+#include <stdio.h>
+
+char checkCase(char b)
+{
+    if (b >= 'a' && b <= 'z')
+    {
+        printf("This is a lowercase character\n");
+    }
+    else
+    {
+        printf("This is not a lowercase character\n");
+    }
+}
+
+int main()
+{
+    char s;
+    
+    printf("Enter a character\n");
+    scanf("%c", &s);
+    
+    checkCase(s);
+    
+    return (0);
+}
