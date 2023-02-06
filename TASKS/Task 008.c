d34d87e54144a790306192ec569710b0b9e9f8d3
JCTNIG <108359199+JCTNIG@users.noreply.github.com>
2023-02-06 22:55:48 +0100

    Update Task 008.c

--- TASKS/Task 008.c
+++ TASKS/Task 008.c
@@ -2,3 +2,20 @@
 Write a function that prints the alphabet, in lowercase
 followed by a new line.
 */
+
+#include <stdio.h>
+
+char upperAlpha()
+{
+    for (char i = 'a'; i <= 'z'; ++i)
+    {
+        printf("%c\n", i);
+    }
+}
+
+int main()
+{
+    upperAlpha();
+    
+    return (0);
+}
