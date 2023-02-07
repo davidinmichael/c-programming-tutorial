153ea76ba4f6e8c21a82c62cbc6b1dad672c58f5
JCTNIG <108359199+JCTNIG@users.noreply.github.com>
2023-02-07 18:32:32 +0100

    Update Task 012.c

--- TASKS/Task 012.c
+++ TASKS/Task 012.c
@@ -3,28 +3,3 @@ Write a function that adds two integers and prints
 the result.
 */
 
-#include <stdio.h>
-int absNum(int n)
-{
-    int abs;
-    if (n < 0)
-    {
-        abs = n - n - n;
-        printf("The absolute of %d is %d", n, abs);
-    }
-    else
-    {
-        printf("The absolute of %d is %d", n, n);
-    }
-}
-
-int main()
-{
-    int num;
-    printf("Enter a number:\n");
-    scanf("%d", &num);
-    
-    absNum(num);
-    
-    return (0);
-}
