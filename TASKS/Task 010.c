414364f1289bbe297103330de4389e24bb5067a2
JCTNIG <108359199+JCTNIG@users.noreply.github.com>
2023-02-07 18:31:40 +0100

    Update Task 010.c

--- TASKS/Task 010.c
+++ TASKS/Task 010.c
@@ -3,3 +3,29 @@ Write a function that computes the absolute
 value of an integer.
 Understand what an absolute value is, before attempting it.
 */
+
+#include <stdio.h>
+int absNum(int n)
+{
+    int abs;
+    if (n < 0)
+    {
+        abs = n - n - n;
+        printf("The absolute of %d is %d", n, abs);
+    }
+    else
+    {
+        printf("The absolute of %d is %d", n, n);
+    }
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number:\n");
+    scanf("%d", &num);
+    
+    absNum(num);
+    
+    return (0);
+}
