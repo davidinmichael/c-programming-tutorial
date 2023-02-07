19c850c2af46a1b2269aa3f7614f61a704921acb
JCTNIG <108359199+JCTNIG@users.noreply.github.com>
2023-02-07 19:21:05 +0100

    Update Task 012.c

--- TASKS/Task 012.c
+++ TASKS/Task 012.c
@@ -3,3 +3,20 @@ Write a function that adds two integers and prints
 the result.
 */
 
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    int sum;
+    sum = a + b;
+    printf("%d + %d = %d\n", a, b, sum);
+}
+
+int main()
+{
+    int n1, n2;
+    printf("Enter two numbers to add\n");
+    scanf("%d \n %d", &n1, &n2);
+    
+    add(n1, n2);
+}
