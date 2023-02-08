c4890cc29e7ef84778a69ae0baa4b3e2bf1b799b
BEChukwunwike <blessingechukwunwike@gmail.com>
2023-02-08 23:18:22 +0100

    New commits

--- TASKS/Task 017.c
+++ TASKS/Task 017.c
@@ -9,3 +9,19 @@
     Output:
     21      // That is; 1+2+3+4+5+6 = 21
 */
+#include <stdio.h>
+
+int sumAll(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    return n + sumAll(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("The sum of integers from 1 to %d is %d", num, sumAll(num));
+    return 0;
+}
