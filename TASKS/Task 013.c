806e09db56264f78511c9e063f57485ba86c8eb2
JCTNIG <108359199+JCTNIG@users.noreply.github.com>
2023-02-08 16:18:41 +0100

    Update Task 013.c

--- TASKS/Task 013.c
+++ TASKS/Task 013.c
@@ -4,3 +4,27 @@ followed by a new line.
 That is n is your argument, it should print from any number
 passed as argument till 100.
 */
+
+#include <stdio.h>
+
+int natNum(int n)
+{
+    for (int i = 0; i <= 100; ++i)
+    {   
+        if (n <= i)
+        {
+            printf("%d\t", i);
+        }
+    }
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number to count till 100\n");
+    scanf("%d", &num);
+    
+    natNum(num);
+    
+    return (0);
+}
