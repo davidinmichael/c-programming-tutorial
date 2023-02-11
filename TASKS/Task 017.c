69cc74db5f6692e13bad814c32e8efaef023ce16
JCTNIG <108359199+JCTNIG@users.noreply.github.com>
2023-02-11 22:58:58 +0100

    Update Task 017.c

--- TASKS/Task 017.c
+++ TASKS/Task 017.c
@@ -9,3 +9,30 @@
     Output:
     21      // That is; 1+2+3+4+5+6 = 21
 */
+
+#include <stdio.h>
+
+int recFunc(int n)
+{
+
+    if (n == 1)
+    {
+        return n;
+    }
+    else
+    {
+        return n + recFunc(n-1);
+    
+    }
+    
+}
+
+int main()
+{
+    int num, sum;
+    printf("Enter a number\n");
+    scanf("%d", &num);
+    sum = recFunc(num),
+    
+    printf("%d", sum);
+}
