550c74968fe8e0ed3ec9a55b617ef0e643f5fac8
JCTNIG <108359199+JCTNIG@users.noreply.github.com>
2023-02-15 13:49:58 +0100

    Update Task-22.c

--- TASKS/Task-22.c
+++ TASKS/Task-22.c
@@ -3,3 +3,14 @@ Create a program that declares a pointer to an integer, assigns an
 address to it, and outputs the value stored at the memory address
 pointed by the pointer.
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int age = 86;
+    int *ptr;
+    ptr = age;
+    printf("%d", age);
+  return (0);
+}
