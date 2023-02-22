f93ba2a6415a02a9790966671daa12b9d22705fe
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-02-23 00:20:53 +0100

    Update Task 004.c

--- TASKS/Task 004.c
+++ TASKS/Task 004.c
@@ -12,3 +12,17 @@ Output:
 Your name is David and you are 65 years old.
 
 */
+#include <stdio.h>
+
+int main() {
+    char n[50];
+    int a;
+//where n represents the name of user, while a represents the user's age
+    printf("Enter Your name: \n");
+    scanf("%s", n);
+    printf("Enter your Age: \n");
+    scanf("%d", &a);
+    printf("Your name is %s and you are %d years old.\n", n, a);
+    
+      return 0;
+}
