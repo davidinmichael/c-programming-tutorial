f6cee627e1495a97a7739976acd69e6f32a8f20b
Fitsum-Kebede <fitsuminfome@gmail.com>
2023-02-22 23:06:48 +0300

    Task 003

---
+++ TASKS/Task003.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 sucess
+ */
+
+int main(void)
+{
+	int num;
+
+	printf("Please input a number: ");
+	scanf("%i", &num);
+
+	if (num % 2 == 0)
+	{
+	printf("User Input is even\n");
+	}
+	else if (num % 2 == 1)
+	{
+	printf("User Input is odd\n");
+	}
+	else if (num == 0)
+	{
+	printf("This is Zero\n");
+	}
+
+	return (0);
+}
