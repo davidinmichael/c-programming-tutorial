303b6897e6b79baab02e12de6acd824695e9b6f9
Easykeljohn <Easykelchimdikejohn@gmail.com>
2023-02-22 22:39:08 +0000

    task 002 and 003 completed

--- TASKS/Task 002.c
+++ TASKS/Task 002.c
@@ -4,3 +4,38 @@ If the score is less than 80, display to the user that they
 are not elgible to be enrolled.
 If the score is greater than or equal 80, they can be enrolled.
 */
+
+#include <stdio.h>
+
+
+
+int main() {
+    
+    
+
+int score;
+    
+
+printf("Enter your Score: ");
+    
+scanf("%d", &score);
+    
+    
+
+if (score < 80){
+        
+printf("you are not eligible to be enrolled");
+    
+}
+    
+    
+if (score >= 80){
+        
+printf("you are eligible to be enrolled");
+    
+}
+    
+
+return 0;
+
+}
--- TASKS/Task 003.c
+++ TASKS/Task 003.c
@@ -7,3 +7,46 @@ If the number is zero, display 'This is zero'
 NOTE: 'userInput' should be the number entered by the user.
 
 */
+
+
+#include <stdio.h>
+
+
+int main() {
+    
+    
+
+int Number;
+    
+    
+printf("Enter your number: ");
+    
+scanf("%d", &Number);
+    
+    
+
+if (Number%2==0){
+        
+printf("Number is even");
+    
+}
+    
+
+    
+else if (Number%2==1){
+        
+printf("Number is odd");
+    
+}
+    
+
+if (Number==0){
+        
+printf("NUmber is zero");
+    
+}
+    
+
+return 0;
+
+}
