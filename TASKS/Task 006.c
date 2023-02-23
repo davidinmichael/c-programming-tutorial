74c796b56697f1859467e26b388c073fd5763c27
John Chimdike Ezekiel <easykelchimdikejohn@gmail.com>
2023-02-23 21:37:20 +0000

    Update Task 006.c

--- TASKS/Task 006.c
+++ TASKS/Task 006.c
@@ -9,27 +9,14 @@ or odd using the function.
 
 int main() 
 {
-
-
 int number;
-
-
 printf("Enter your number: ");
-
 scanf("%d", &number);
 
-
-
 if (number%2==0){
-    
 printf("Number is even");}
-
 else {    
-    
 printf("Number is odd");}
 
-
-
 return 0;
-
 }
