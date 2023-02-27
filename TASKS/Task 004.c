82ddabc07b81d794ff1e6693660ef179ddb95721
OthibengKgalaletso <othibengkgalaletso3@gmail.com>
2023-02-27 12:33:52 +0200

    Tasks 4 and 6

--- TASKS/Task 004.c
+++ TASKS/Task 004.c
@@ -10,5 +10,19 @@ How old are you?: 65
 
 Output:
 Your name is David and you are 65 years old.
-
 */
+#include <stdio.h>
+
+int main(void)
+{
+	char name *[12];
+	int age;
+
+	printf("What is your name: ");
+	scanf("%s", &name);
+
+	printf("How old are you?: ");
+	scanf("%d", &age);
+
+	printf("Your name is %s and you are %d years old\n", name, age);
+}	
--- TASKS/Task 006.c
+++ TASKS/Task 006.c
@@ -2,3 +2,20 @@
 Write a program in C to check a given number is even
 or odd using the function.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	
+	printf("Please enter a number: ");
+	scanf("%d", &num);
+
+	if (num % 2 == 0)
+	{
+		printf("%d is even \n", num);
+	}
+	else 
+		printf("%d is odd \n", num);
+}
