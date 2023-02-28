7556ea1bc8052fefa44b643a077a60a06b6f485e
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-02-28 11:06:21 +0100

    Thanks david

--- TASKS/Task 007.c
+++ TASKS/Task 007.c
@@ -3,3 +3,14 @@ write your solution after the comment
 Write a function that prints the alphabet, in uppercase,
 followed by a new line.
 */
+#include <stdio.h>
+void capital_letters()
+{
+    for (char i = 'A'; i <= 'Z'; ++i)
+    printf("%c\n", i);
+}
+int main()
+{
+    capital_letters();
+    return (0);
+}
