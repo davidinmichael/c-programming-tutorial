c408d2e637a82aa06401e5dac5c67a9c64872f28
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-02-28 11:19:41 +0100

    Thanks David

--- TASKS/Task 008.c
+++ TASKS/Task 008.c
@@ -2,3 +2,14 @@
 Write a function that prints the alphabet, in lowercase
 followed by a new line.
 */
+#include <stdio.h>
+void capital_letters()
+{
+    for (char i = 'a'; i <= 'z'; ++i)
+    printf("%c\n", i);
+}
+int main()
+{
+    capital_letters();
+    return (0);
+}
