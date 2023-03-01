9b7662270be7315ffbdf2cb1b4f91cd70836c441
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-03-01 22:37:58 +0100

    Update Task 009.c

--- TASKS/Task 009.c
+++ TASKS/Task 009.c
@@ -3,3 +3,20 @@ Write a function that checks for lowercase character.
 That is, when passed in an arguments, it checks if the
 argument is lowercase or uppercase.
 */
+// Online C compiler to run C program online
+#include <stdio.h>
+void zero_to_14_10x()
+{
+    int x,y;
+    for(x >= 0; x <= 14; ++x)
+    {
+        for(y=0; y<15; y++)
+    printf("%d\n", x);
+    }
+}
+int main() 
+{
+    zero_to_14_10x();
+
+    return 0;
+}
