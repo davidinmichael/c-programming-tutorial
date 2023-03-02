de242d67c2597c364f32a24679b4a2b315f42005
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-03-02 07:04:21 +0100

    Update Task 012.c

--- TASKS/Task 012.c
+++ TASKS/Task 012.c
@@ -2,3 +2,16 @@
 Write a function that adds two integers and prints
 the result.
 */
+#include <stdio.h>
+void sum()
+{
+    int x;
+    int y;
+    if (x = 9, y=8)
+    printf("%d + %d = %d\n", x, y,x+y);
+}
+int main()
+{
+    sum();
+    return 0;
+}
