e7dcd38d6602b221a678bf36b2c476b6612f93e5
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-03-02 07:00:39 +0100

    Going

--- TASKS/Task 011.c
+++ TASKS/Task 011.c
@@ -7,3 +7,17 @@ Output should look like this;
 9 x 2 = 18
 ... and so on till 12
 */
+#include <stdio.h>
+void table_9()
+{
+    int x;
+    int y;
+    if (x = 9)
+    for (y>=0; y<=12; y++)
+    printf("%d X %d = %d\n", x, y,x*y);
+}
+int main()
+{
+    table_9();
+    return 0;
+}
