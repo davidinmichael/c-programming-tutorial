90d8c5290e46f7d7e956a27235fa8a631a3d1a12
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-03-02 07:10:45 +0100

    GOing

--- TASKS/Task 012.c
+++ TASKS/Task 012.c
@@ -7,6 +7,25 @@ void sum()
 {
     int x;
     int y;
+    printf("Enter an integer: ");
+    scanf("%d", &x);
+    printf("Enter another integer you want to add: ");
+    scanf("%d", &y);
+    printf("%d + %d = %d\n", x, y,x+y);
+}
+int main()
+{
+    sum();
+    return 0;
+}
+
+OR
+
+#include <stdio.h>
+void sum()
+{
+    int x;
+    int y;
     if (x = 9, y=8)
     printf("%d + %d = %d\n", x, y,x+y);
 }
