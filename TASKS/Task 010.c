5339f421ca75202bf6f6e6000372321eafea9d85
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-03-02 06:33:37 +0100

    going

--- TASKS/Task 010.c
+++ TASKS/Task 010.c
@@ -3,3 +3,14 @@ Write a function that computes the absolute
 value of an integer.
 Understand what an absolute value is, before attempting it.
 */
+#include <stdio.h>
+
+int abs(int x) {
+    return x < 0 ? -x : x;
+}
+
+int main() {
+    int x = -5;
+    printf("The absolute value of %d is %d.\n", x, abs(x));
+    return 0;
+}
